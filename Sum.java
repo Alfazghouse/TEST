ef39c62fac6b6aafa414e006df063f2eeaf333e1
alfaz <alfazghouse2104@gmail.com>
2021-01-21 14:16:51 +0530

    new changes added

--- Sum.java
+++ Sum.java
@@ -1,7 +1,8 @@
 public class Sum{
   public static void main(String[] args) {
     int x = 5;
-    int y = 6;
-    System.out.println( int z = x + y); 
+    int y = 6,z;
+    
+    System.out.println(z= x + y); 
   }
 }
