d45e97cd0ca8505e293f9265b1c8201a8c0c2531
Alfazghouse <57977801+Alfazghouse@users.noreply.github.com>
2021-01-21 12:12:04 +0530

    Updated Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,6 @@ public class Main {
   public static void main(String[] args) {
     int x = 5;
     int y = 6;
-    int z = x + y;
-    System.out.println(z); 
+    System.out.println(int z= x + y); 
   }
 }
